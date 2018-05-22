6aa75ad6e87f2888fd2a5f9624d9551fbe1f07b5
lucas.bulloni@he-arc.ch <lucas.bulloni@he-arc.ch>
2018-05-22 14:58:26 +0200

    vector

---
+++ Vector2.js
@@ -0,0 +1,73 @@
+class Vector2
+{
+  constructor(arg1, arg2, isPolar = false)
+  {
+    if(!isPolar)
+    {
+      this.x = arg1;
+      this.y = arg2;
+
+      this.computePolarVector();
+    }
+    else
+    {
+      this.length = arg1;
+      this.angle = arg2;
+
+      this.computeCartesianVector();
+    }
+
+  }
+
+  computePolarVector()
+  {
+    this.length = Math.sqrt(x*x + y*y);
+    this.angle = Math.atan2(y, x)
+  }
+
+  computeCartesianVector()
+  {
+    this.x = this.length * Math.cos(this.angle);
+    this.y = this.length * Math.sin(this.angle);
+  }
+
+  setX(x)
+  {
+    this.x = x;
+    this.computePolarVector();
+  }
+
+  setY(y)
+  {
+    this.y = y;
+    this.computePolarVector();
+  }
+
+  setAngle(angle)
+  {
+    this.angle = angle;
+    this.computeCartesianVector();
+  }
+
+  setLength(length)
+  {
+    this.length = length;
+    this.computeCartesianVector();
+  }
+
+  add(vect)
+  {
+    return new Vector2(this.x + vect.x, this.y + vect.y);
+  }
+
+  sub(vect)
+  {
+    vect = vect.multConstant(-1);
+    return new Vector2(vect.x + this.x, vect.y + this.y);
+  }
+
+  multConstant(constant)
+  {
+    return new Vector2(this.x * constant, this.y * constant);
+  }
+}
