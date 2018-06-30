bc2f29e6ef584f154ca41293e05ba3edcd2a783e
lucas.bulloni@he-arc.ch <lucas.bulloni@he-arc.ch>
2018-06-30 17:34:59 +0200

    all planet added, changing how calculate the speed and distance, now all planets interacts with the others, adding information, adding some field to change the parameter of the simulation

--- script.js
+++ script.js
@@ -1,8 +1,46 @@
 import { Simulation } from './Simulation.js';
 
+var simulation = undefined;
+
+function startSimulation()
+{
+  if(simulation !== undefined)
+  {
+    simulation.start();
+  }
+}
+
+function stopSimulation()
+{
+  if(simulation !== undefined)
+  {
+    simulation.stop();
+  }
+}
+
+function resetSimulation()
+{
+  setCanvasSize();
+  if(simulation !== undefined)
+  {
+    simulation.stop();
+  }
+  simulation = new Simulation(document.getElementById('Simulation'), document.getElementById('daysFrame').value, document.getElementById('pixelsPerAU').value);
+}
+
+function setCanvasSize()
+{
+  document.getElementById('Simulation').width = document.getElementById('widthCanvas').value;
+  document.getElementById('Simulation').height = document.getElementById('heightCanvas').value;
+}
+
 document.addEventListener('DOMContentLoaded', function(event) {
-  let canvas = document.getElementById('Simulation');
-  let simulation = new Simulation(canvas);
-  simulation.start();
-  //simulation.stop();
+  document.getElementById("btnStart").addEventListener('click', startSimulation, false);
+  document.getElementById("btnStop").addEventListener('click', stopSimulation, false);
+  document.getElementById("btnReset").addEventListener('click', resetSimulation, false);
+  document.getElementById("btnConfigure").addEventListener('click', resetSimulation, false);
+
+  setCanvasSize();
+
+  resetSimulation();
 });
